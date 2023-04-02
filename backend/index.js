9a7c3a48925f477f99287de6f1e2e6d0276f3c2c
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2023-04-02 15:47:24 +0530

    Backend Functionality for Multi-Device Detection

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const body_parser = require('body-parser')
 app.use(body_parser.urlencoded({ extended: true }))
 app.use(body_parser.json());
 
-// const cjson = require('circular-json') // for debugging purposes
+const cjson = require('circular-json') // for debugging purposes
 
 const port = env.PORT
 
@@ -60,20 +60,22 @@ app.get('/current_status', (req, res) => {
 app.post('/current_status', (req, res) => {
     var req_datetime =  moment().format("YYYY-MM-DD HH:mm:ss")
     console.log("DATETIME: " + req_datetime)
-    
-    console.log("Received POST Request for current status. Payload: " + req.body.is_present)
+
+    console.log("BODY OF POST REQUEST: ", JSON.stringify(req.body))
+
+    console.log("Received POST Request for current status.\nPAYLOAD -> is_present: " + req.body["is_present"] + ", n_devices_found: " + req.body["n_devices_found"])
+    console.log("ARRAY OF DEVICES: " + req.body["devices_found"])
     
     // const received_req = cjson.stringify(req)                // for debugging purposes
     // console.log("RECEIVED REQUEST: " + received_req) 
-    
-    // var valueOfDevice = req.headers['data'] == "1" ? 1 : 0   // alternate method
+    // res.status(200).send({})
 
     var success = true
 
     db.query(
 
         "UPDATE user_detected SET is_present=? WHERE time_stamp='2001-01-01 00:00:00'",
-        req.body.is_present, 
+        req.body["is_present"], 
 
         (err, result) => {
             if(err)
@@ -98,7 +100,7 @@ app.post('/current_status', (req, res) => {
         db.query(
 
             "INSERT INTO user_detected(time_stamp, is_present) VALUES(?,?)",
-            [req_datetime, req.body.is_present],
+            [req_datetime, req.body["is_present"]],
             
             (err, result) => {
                 if(err)
@@ -124,4 +126,38 @@ app.post('/current_status', (req, res) => {
             message: "All The Values successfully updated in the Database."
         })
     }
+})
+
+
+app.get("/ble_mac", (req, res) => {
+    db.query(
+
+        "SELECT mac_address FROM ble_mac_addresses",
+
+        (err, result) => {
+            if(err)
+            {
+                console.log("Values couldn't be read from the Database.\nERROR: " + err)
+                res.send({
+                    error: err,
+                    message: "Request couldn't be granted."
+                })
+            }
+            else
+            {
+                console.log("Values obtained from the Database successfully.\nRESULT: Devices Under Observation: " + result)
+                var devicesOfInterest = [];
+                for(let i = 0 ; i < result.length ; ++i)
+                {
+                    devicesOfInterest.push(result[i].mac_address)
+                    console.log(devicesOfInterest[i])
+                }
+                
+                res.status(200).send({
+                    devices: devicesOfInterest,
+                    message: "Request executed successfully."
+                })
+            }
+        }
+    )
 })
