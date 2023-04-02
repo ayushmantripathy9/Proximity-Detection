9cd8c994d84aa73e0557461967fa27afad4a1e61
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2023-04-03 03:27:29 +0530

    Frontend code for Data Analysis

--- frontend/proximity-detection/src/components/analysis/index.js
+++ frontend/proximity-detection/src/components/analysis/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import TableComponent from './table_component';
 
 function Analysis() {
   return (
     <div>
       <h1>Welcome to my Data Analysis!</h1>
       <p>Data Analysis coming up soon ......</p>
+
+      {<TableComponent />}
     </div>
   );
 }
